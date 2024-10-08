51d65ee0e2d082fa3756e05a2584c8c6a40e1aee
sorrymorning <init.defaultBranch>
2024-10-08 22:08:36 +0300

    Done my lab

---
+++ ThreadLab/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        class myThread
+        {
+            Thread thread;
+
+            public myThread(string name, int num) //Конструктор получает имя функции и номер до кторого ведется счет
+            {
+
+                thread = new Thread(this.func);
+                thread.Name = name;
+                thread.Start(num);//передача параметра в поток
+            }
+
+            void func(object num)//Функция потока, передаем параметр
+            {
+                for (int i = 0; i < (int)num; i++)
+                {
+                    Console.WriteLine(Thread.CurrentThread.Name + " выводит " + i);
+                    Thread.Sleep(0);
+                }
+                Console.WriteLine(Thread.CurrentThread.Name + " завершился");
+            }
+
+
+        }
+
+        static void Main(string[] args)
+        {
+            myThread t1 = new myThread("Thread 1", 6);
+            myThread t2 = new myThread("Thread 2", 3);
+            myThread t3 = new myThread("Thread 3", 2);
+
+            Console.Read();
+
+        }
+
+    }
+}
